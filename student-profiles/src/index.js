7fa25f0aff967fa8c28abdd46b1d26fe1c8cde83
SullyDurgin <katsdurgin@gmail.com>
2022-02-02 11:39:18 -0500

    react app start

---
+++ student-profiles/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function HelloWorld() {
+	return <div>hello world</div>
+}
+
+ReactDOM.render(<HelloWorld />, document.querySelector('#root'))
